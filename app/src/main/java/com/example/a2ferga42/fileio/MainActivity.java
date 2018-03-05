870c510972b13982f7bff21100ee8156735af8f9
2ferga42 <2ferga42@solent.ac.uk>
2018-03-05 12:37:59 +0000

    Started coding

--- app/src/main/java/com/example/a2ferga42/fileio/MainActivity.java
+++ app/src/main/java/com/example/a2ferga42/fileio/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.a2ferga42.fileio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public boolean onCreateOptionsMenu(Menu, menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.(whatever here))
+        {
+            Intent intent = new Intent(this, (whatever here));
+            startActivityForResult(intent, 0);
+        }
+
+    }
 }
+
